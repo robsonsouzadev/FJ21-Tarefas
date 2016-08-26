fc77ad70a2df99ef3f6a3dec15f10f6abf9671e9
Fábio Ueno <fabio.t.ueno@gmail.com>
2016-08-26 14:35:56 -0300

    [Exercise 13.5] Inversion of control with Spring Container
    Used Spring IoC to inject the DataSource and JdbcTarefasDao.

--- src/br/com/caelum/tarefas/controller/TarefasController.java
+++ src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -2,6 +2,7 @@ package br.com.caelum.tarefas.controller;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,6 +15,13 @@ import br.com.caelum.tarefas.modelo.Tarefa;
 @Controller
 public class TarefasController {
 
+	private final JdbcTarefaDao dao;
+
+	@Autowired
+	public TarefasController(JdbcTarefaDao dao) {
+		this.dao = dao;
+	}
+
 	@RequestMapping("novaTarefa")
 	public String form() {
 		return "tarefa/formulario";
@@ -25,7 +33,6 @@ public class TarefasController {
 			return "tarefa/formulario";
 		}
 
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.adiciona(tarefa);
 
 		return "tarefa/adicionada";
@@ -33,7 +40,6 @@ public class TarefasController {
 
 	@RequestMapping("listaTarefas")
 	public String lista(Model model) {
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		model.addAttribute("tarefas", dao.lista());
 
 		return "tarefa/lista";
@@ -42,7 +48,6 @@ public class TarefasController {
 	@ResponseBody
 	@RequestMapping("removeTarefa")
 	public void remove(Tarefa tarefa) throws InterruptedException {
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.remove(tarefa);
 
 		Thread.sleep(5000);
@@ -50,7 +55,6 @@ public class TarefasController {
 
 	@RequestMapping("mostraTarefa")
 	public String mostra(Long id, Model model) {
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		Tarefa tarefa = dao.buscaPorId(id);
 
 		model.addAttribute("tarefa", tarefa);
@@ -60,7 +64,6 @@ public class TarefasController {
 
 	@RequestMapping("alteraTarefa")
 	public String altera(Tarefa tarefa) {
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.altera(tarefa);
 
 		return "redirect:listaTarefas";
@@ -68,7 +71,6 @@ public class TarefasController {
 
 	@RequestMapping("finalizaTarefa")
 	public String finaliza(Long id, Model model) throws InterruptedException {
-		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.finaliza(id);
 
 		model.addAttribute("tarefa", dao.buscaPorId(id));
